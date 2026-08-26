93e59421e0c8c4714cd33afd9d4706e79ac221b3
rouiller38023-netizen <rouiller38023@students.apsb.org>
2026-08-26 19:25:20 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,24 +27,59 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+    //toggleGrid();
 
 
     // TODO 2 - Create Platforms
-
-
-
-
+    createPlatform(0, 700, 1400, 50, "green");
+    createPlatform(0, 200, 10, 800, "green");
+    createPlatform(150, 200, 10, 800, "green");
+    createPlatform(120, 600, 40, 10, "limegreen");
+    createPlatform(0, 500, 40, 10, "limegreen");
+    createPlatform(120, 400, 40, 10, "limegreen");
+    createPlatform(0, 300, 40, 10, "limegreen");
+    createPlatform(120, 200, 40, 10, "limegreen");
+    createPlatform(250, 0, 10, 500, "green");
+    createPlatform(150, 650, 140, 50, "green");
+    createPlatform(250, 500, 930, 10, "green");
+    createBadPlatform(290, 690, 980, 10, "red");
+    createPlatform(375, 650, 30, 10, "limegreen");
+    createPlatform(500, 650, 30, 10, "limegreen");
+    createPlatform(625, 650, 30, 10, "limegreen");
+    createPlatform(750, 650, 30, 10, "limegreen");
+    createPlatform(875, 650, 30, 10, "limegreen");
+    createPlatform(1000, 650, 30, 10, "limegreen");
+    createPlatform(1125, 650, 30, 10, "limegreen");
+    createPlatform(1250, 625, 30, 10, "limegreen");
+    createPlatform(1270, 550, 140, 300,"green");
+    createPlatform(350, 350, 1400, 10,"green");
+    createPlatform(250, 450, 40, 10, "limegreen");
+    createPlatform(400, 300, 100, 50, "green");
+    createBadPlatform(500, 340, 800, 10, "red");
+    createPlatform(1300, 250, 100, 100, "green");
+    createPlatform(6500, 275, 30, 10, "limegreen");
+    createPlatform(525, 275, 30, 10, "limegreen");
+    createPlatform(625, 275, 30, 10, "limegreen");
+    createPlatform(750, 275, 30, 10,"limegreen");
+    createPlatform(875, 275, 30, 10,"limegreen");
+    createPlatform(1000, 275, 30, 10,"limegreen");
+    createPlatform(1125, 275, 30, 10,"limegreen");
+    createPlatform(1250, 275, 30, 10,"limegreen");
     // TODO 3 - Create Collectables
-
-
-
-    
+    createCollectable("database", 50, 650)
+    createCollectable("database", 1350, 200)
+    createCollectable("database", 750, 450)
+    createCollectable("database", 1350, 500)
     // TODO 4 - Create Cannons
-
-
-    
-    
+    createCannon("top", 125, 1000)
+    createCannon("top", 500, 900)
+    createCannon("top", 650, 900)
+    createCannon("top", 750, 900)
+    createCannon("top", 900, 900)
+    createCannon("top", 1000, 900)
+    createCannon("top", 1150, 900)
+    createCannon("top", 1250, 900)
+    createCannon("top", 1375, 1000)
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
     //////////////////////////////////
